62f5b7b0b256979e882fe7284a592664f2d6fb17
= <=>
2026-07-01 16:40:15 -0500

    Añadie el ejercicio 4

---
+++ ejercicio_4.js
@@ -0,0 +1,8 @@
+function calcularDefinitiva(nota1, nota2, nota3){
+    let totalNota1 = nota1*0.2;
+    let totalNota2 = nota2*0.3;
+    let totalNota3 = nota3*0.5;
+    return totalNota1+totalNota2+totalNota3;
+}
+
+console.log( "la nota definitiva del aprendiz es: " + calcularDefinitiva(3.4, 4, 5));
