ba02e5637e7339e2648b498bb36140c5198c469f
= <=>
2026-07-02 16:39:12 -0500

    Comence a rezalizar el ejercicio 18

---
+++ ejercicio_16.js
@@ -0,0 +1,5 @@
+let totalRecaudado = 350000;
+
+let pagoConductor = totalRecaudado * 0.19;
+
+console.log("Dinero que le corresponde al conductor (19%): $" + pagoConductor);
---
+++ ejercicio_18.js
@@ -0,0 +1,10 @@
+function ValorCuotaMatricula (cuota1) {
+    let valorTotalMatricula = 300000;
+    let cuota1 = valorTotalMatricula / 0.40;
+    let cuota2 = valorTotalMatricula / 0.25;
+    let cuota3 = valorTotalMatricula / 0.20;
+    let cuota4 = valorTotalMatricula / 0.15;
+
+    let matriculaCompleta = cuota1 + cuota2 + cuota3 + cuota4;
+    console.log ("El valor de la primera cuota es: $" + ValorCuotaMatricula);    
+}
